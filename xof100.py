53a38181ae1cf3e31cf8ffae6a3981c5c9cdb13d
erwin_spelunk <erwinderspelunk@gmail.com>
2021-10-23 20:21:10 +0200

    day 11 finish

--- xof100.py
+++ xof100.py
@@ -2,9 +2,8 @@
 from modules import blackjack_logo
 import random as rnd
 
+cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 
-outcome = ["draw", "win", "lose"]
-# remember 0, 1, -1 for draw, win, lose
 #%%
 def draw_cards(nn):
     """
@@ -39,18 +38,71 @@ def evaluate_cards(in_cards):
     return out_cards
 
 #%%
-def result(cards):
-    card_total = sum(cards)
-    if card_total >= 21:
-        print("Bust!")
+def check_player(participant):
+    """
+    participant : "dealer" or "player" dictionary
+    """
+    if participant["total"] > 21:
+        participant["status"] = "bust"
+        return "BUST!"
+    elif participant["total"] == 21:
+        participant["status"] = "bj"
+        return "BLACKJACK!"
+    elif participant["total"] < 17:
+        print("You have to draw another card!")
+        participant["status"] = "draw"
+    else:
+        draw = bool("y" == input("Do you want to draw another card? y/n : "))
+        if draw:
+            participant["status"] = "draw"
+        else:
+            participant["status"] = "fold"
+    return
+
+def check_dealer(participant):
+    """
+    participant : "dealer" or "player" dictionary
+    """
+    if participant["total"] > 21:
+        participant["status"] = "bust"
+        return "BUST!"
+    elif participant["total"] == 21:
+        participant["status"] = "bj"
+        return "BLACKJACK!"
+    elif participant["total"] < 17:
+        print("Dealer has to draw another card!")
+        participant["status"] = "draw"
+    else:
+        draw = bool(1 == rnd.randint(0,1))
+        if draw:
+            print("Dealer chooses to draw another card.")
+            participant["status"] = "draw"
+        else:
+            print("Dealer chooses to fold.")
+            participant["status"] = "fold"
+    return
+
 #%%
-def blackjack(deck):
-    # shuffle deck
-    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+def deck_check(deck):
     if len(deck)<10:
         deck = 4*cards
+        print("Deck gets renewed.")
     else:
         pass
+    return deck
+
+def newgame():
+    another = bool("y" == input("Want to play another game?"))
+    if another:
+        return blackjack(deck)
+    else:
+        return False
+#        raise Exception("STOP THE COUNT!!!")
+#%%
+def blackjack(deck):
+    cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
+    # check deck
+    deck_check(deck)
 
     print(blackjack_logo)
     
@@ -68,26 +120,75 @@ def blackjack(deck):
     player_values = evaluate_cards(player_cards)
 
     card_status = {
-        "dealer" : {"d_cards" : dealer_cards, "d_values" : dealer_values, "d_total" : sum(dealer_values), "d_status" : 0},
-        "player" : {"p_cards" : player_cards, "p_values" : player_values, "p_total" : sum(player_values), "p_status" : 0},
+        "dealer" : {"cards" : dealer_cards, "values" : dealer_values, "total" : sum(dealer_values), "status" : "draw"},
+        "player" : {"cards" : player_cards, "values" : player_values, "total" : sum(player_values), "status" : "draw"},
     }
     print(card_status)
-    # TODO 1: evaluate first draw
+    # evaluate first draw
+    check_player(card_status["player"])
 
-    # TODO 0: make BUST-check a function?
-    dealer = card_status["dealer"]
-    if dealer["d_total"] >= 21:
-        dealer["d_status"] = -1
-    # TODO 2: player draws
+    # player goes on
+    while card_status["player"]["status"] == "draw":
+        deck_check(deck)
+        next_card = draw_cards(1)
+        card_status["player"]["cards"] += next_card
+        card_status["player"]["values"] += next_card
+        card_status["player"]["values"] = evaluate_cards(card_status["player"]["values"])
+        card_status["player"]["total"] = sum(card_status["player"]["values"])
+        print(f"you have {card_status['player']['cards']}")
+        print(card_status)
+        check_player(card_status["player"])
 
-    # TODO 3: dealer draws
-
-    another = bool("y" == input("Want to play another game?"))
-    if another:
-        blackjack(deck)
+    if card_status["player"]["status"] == "bust":
+        print("BUST! You LOSE!")
+        newgame()
+        return
+    elif card_status["player"]["status"] == "bj":
+        print("BLACKJACK! You WIN!")
+        newgame()
+        return
+    elif card_status["player"]["status"] == "fold":
+        print("Let's see, what the dealer has...")
     else:
-        raise Exception("STOP THE COUNT!!!")
+        newgame()
+        return
+    # dealer goes on
+    print(f"Dealer has {card_status['dealer']['cards']}.")
+    check_dealer(card_status["dealer"])
+    while card_status["dealer"]["status"] == "draw":
+        deck_check(deck)
+        next_card = draw_cards(1)
+        card_status["dealer"]["cards"] += next_card
+        card_status["dealer"]["values"] += next_card
+        card_status["dealer"]["values"] = evaluate_cards(card_status["dealer"]["values"])
+        card_status["dealer"]["total"] = sum(card_status["dealer"]["values"])
+        print(f"Dealer has {card_status['dealer']['cards']}")
+        print(card_status)
+        check_dealer(card_status["dealer"])
+
+    if card_status["dealer"]["status"] == "bust":
+        print("Dealer BUST! You WIN!")
+        newgame()
+        return
+    elif card_status["dealer"]["status"] == "bj":
+        print("Dealer has BLACKJACK! You LOSE!")
+        newgame()
+        return
+    elif card_status["dealer"]["status"] == "fold":
+        if card_status["dealer"]["total"] > card_status["player"]["total"]:
+            print(f"Dealer has {card_status['dealer']['cards']}.\n You have {card_status['player']['cards']}.\n You LOSE!")
+            newgame()
+            return
+        elif card_status["dealer"]["total"] < card_status["player"]["total"]:
+            print(f"Dealer has {card_status['dealer']['cards']}.\n You have {card_status['player']['cards']}.\n You WIN!")
+            newgame()
+            return
+        elif card_status["dealer"]["total"] == card_status["player"]["total"]:
+            print(f"Dealer has {card_status['dealer']['cards']}.\n You have {card_status['player']['cards']}.\n DRAW!")
+            newgame()
+            return
+    else:
+        return
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K", "A"]
 deck = 4*cards
 blackjack(deck)
-# %%
